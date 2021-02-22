5bdb01a87c6c0938e05a1c2324bd3a0a574f9831
Mike Ritchie <patrick.ritchie89@gmail.com>
2021-02-22 12:53:56 -0500

    update to music page and experience section cards

--- wp-content/themes/theme-folder/templates/music-template.php
+++ wp-content/themes/theme-folder/templates/music-template.php
@@ -22,9 +22,31 @@
 
 <!--/Hero-->
 
-<audio src="/wp-content/uploads/2021/02/first_piece.m4a" 
-  type="audio/mpeg" 
-  controls>
-  I'm sorry. You're browser doesn't support HTML5 <code>audio</code>.
-</audio>
+<!--Song Section-->
+<?php if (have_rows('music_section')) : ?>
+<?php while (have_rows('music_section')) : the_row(); ?>
+<section class="section song-section">
+  <div class="container">
+    <div class="row song-row">
+      <div class="col-lg-8 sol-md-8 col-sm-4 song-col ">
+        <div class="media-content">
+          <h1><?php the_sub_field('title'); ?></h1>
+        </div>
+        <div class="media-player">
+          <audio src="/wp-content/uploads/2021/02/first_piece.m4a" 
+                  type="audio/mpeg" 
+                  controls>
+                  I'm sorry. You're browser doesn't support HTML5 <code>audio</code>.
+          </audio>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php endwhile; ?>
+<?php endif; ?>
+
+<!--/Song Section-->
+
+
 <?php get_footer(); ?>
