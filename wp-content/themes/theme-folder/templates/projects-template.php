8a867b7c959c43c40d0b6751eed78a8422a06b44
Mike Ritchie <patrick.ritchie89@gmail.com>
2021-03-10 13:57:47 -0500

    desktop styling wrapped up

--- wp-content/themes/theme-folder/templates/projects-template.php
+++ wp-content/themes/theme-folder/templates/projects-template.php
@@ -29,18 +29,20 @@
     <?php if (have_rows('project_one')) : ?>
     <?php while (have_rows('project_one')) : the_row(); ?> 
     <div class="row project-row">
-        <div class="col-5 project-col"> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/ott_3.png" alt="website screen shot"/> 
-            </div> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/ott_2.png"   alt="website screen shot"/>
-            </div>
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/ott_1.png"   alt="website screen shot"/>
+        <div class="col-lg-6 project-col"> 
+            <div class="wrapper">
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/ott_3.png" alt="website screen shot"/> 
+                </div> 
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/ott_2.png"   alt="website screen shot"/>
+                </div>
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/ott_1.png"   alt="website screen shot"/>
+                </div>
             </div>
         </div>
-        <div class="col-5 content-col">  
+        <div class="col-lg-6 content-col">  
             <h3><?php the_sub_field('title'); ?></h3>
             <p class="content"><?php the_sub_field('content');?></p>
         </div>
@@ -51,21 +53,24 @@
     <?php if (have_rows('project_two')) : ?>
     <?php while (have_rows('project_two')) : the_row(); ?> 
     <div class="row project-row">
-        <div class="col-5 project-col"> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/emindful_1.png" alt="website screen shot"/> 
-            </div> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/emindful_2.png"   alt="website screen shot"/>
-            </div>
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/emindful_3.png"   alt="website screen shot"/>
+        <div class="col-lg-6 project-col"> 
+            <div class="wrapper">
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/emindful_1.png" alt="website screen shot"/> 
+                </div> 
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/emindful_2.png"   alt="website screen shot"/>
+                </div>
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/emindful_3.png"   alt="website screen shot"/>
+                </div>
             </div>
         </div>
-        <div class="col-5 content-col">  
+        <div class="col-lg-6 content-col">  
             <h3><?php the_sub_field('title'); ?></h3>
             <p class="content"><?php the_sub_field('content');?></p>
         </div>
+   
     </div>
     <?php endwhile; ?>
     <?php endif; ?>
@@ -73,18 +78,20 @@
     <?php if (have_rows('project_three')) : ?>
     <?php while (have_rows('project_three')) : the_row(); ?>      
     <div class="row project-row">
-        <div class="col-lg-5 col-md-4 col-sm-4 project-col img-col-one"> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/sscf_1.png" alt="website screen shot"/> 
-            </div> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/sscf_2.png"   alt="website screen shot"/>
-            </div>
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/sscf_3.png"   alt="website screen shot"/>
+        <div class="col-lg-6  project-col"> 
+            <div class="wrapper">
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/sscf_1.png" alt="website screen shot"/> 
+                </div> 
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/sscf_2.png"   alt="website screen shot"/>
+                </div>
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/sscf_3.png"   alt="website screen shot"/>
+                </div>
             </div>
         </div>
-        <div class="col-5 content-col">  
+        <div class="col-lg-6 content-col">  
             <h3><?php the_sub_field('title'); ?></h3>
             <p class="content"><?php the_sub_field('content');?></p>
         </div>
@@ -95,21 +102,23 @@
     <?php if (have_rows('project_four')) : ?>
     <?php while (have_rows('project_four')) : the_row(); ?>
     <div class="row project-row">
-        <div class="col-5 project-col"> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/bio_1.png" alt="website screen shot"/> 
-            </div> 
-            <div class="img-wrapper">
-                <img src="/wp-content/uploads/2021/03/bio_2.png"   alt="website screen shot"/>
-            </div>
-            <div class="img-wrapper mobile-img">
-                <img src="/wp-content/uploads/2021/03/bio_mobile1.png"   alt="website screen shot"/>
-            </div>
-            <div class="img-wrapper mobile-img">
-                <img src="/wp-content/uploads/2021/03/bio_mobile2.png"   alt="website screen shot"/>
+        <div class="col-lg-6 project-col"> 
+            <div class="wrapper">
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/bio_1.png" alt="website screen shot"/> 
+                </div> 
+                <div class="img-wrapper">
+                    <img src="/wp-content/uploads/2021/03/bio_2.png"   alt="website screen shot"/>
+                </div>
+                <div class="img-wrapper mobile-img">
+                    <img src="/wp-content/uploads/2021/03/bio_mobile1.png"   alt="website screen shot"/>
+                </div>
+                <div class="img-wrapper mobile-img">
+                    <img src="/wp-content/uploads/2021/03/bio_mobile2.png"   alt="website screen shot"/>
+                </div>
             </div>
         </div>
-        <div class="col-5 content-col">  
+        <div class="col-lg-6 content-col">  
             <h3><?php the_sub_field('title'); ?></h3>
             <p class="content"><?php the_sub_field('content');?></p>
         </div>
