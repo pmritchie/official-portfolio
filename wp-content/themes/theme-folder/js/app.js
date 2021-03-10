e19cdeb175a225a7189a92a92cb3378236f8e8d1
Mike Ritchie <patrick.ritchie89@gmail.com>
2021-03-10 14:16:21 -0500

    mobile styling home page completed

--- wp-content/themes/theme-folder/js/app.js
+++ wp-content/themes/theme-folder/js/app.js
@@ -1264,57 +1264,60 @@ $(document).ready(function () {
     var check_if_in_view = function check_if_in_view() {
       var window_top_position = $window.scrollTop();
       var bxTwo = $animation_trigger.offset();
+      var windowWidth = $(window).width();
       var bxTwoTrigger = bxTwo.top - bxTwo.top * .3;
       console.log(bxTwoTrigger);
 
-      if (bxTwoTrigger <= window_top_position) {
-        $(".box").css({
-          "animation": "fadeIn ease 1s",
-          "-webkit-animation": "fadeIn ease 1s",
-          "-moz-animation": "fadeIn ease 1s",
-          "-o-animation": "fadeIn ease 1s",
-          "-ms-animation": "fadeIn ease 1s"
-        });
-        $("#box-one").animate({
-          left: "10%"
-        }, 1000);
-        $("#box-one").css({
-          '-webkit-transform': 'rotate(' + -30 + 'deg)',
-          '-moz-transform': 'rotate(' + -30 + 'deg)',
-          '-ms-transform': 'rotate(' + -30 + 'deg)',
-          'transform': 'rotate(' + -30 + 'deg)'
-        });
-        $("#box-two").animate({
-          left: "10%",
-          top: "40%"
-        }, 1000);
-        $("#box-three").animate({
-          right: "10%",
-          top: "15%"
-        }, 1000);
-        $("#box-four").animate({
-          right: "11%",
-          top: "35%"
-        }, 1000);
-        $("#box-five").animate({
-          right: "10%",
-          bottom: "25%"
-        }, 1000);
-        $("#box-six").animate({
-          left: "10%",
-          bottom: "25%"
-        }, 1000);
-        $("#box-six").css({
-          'box-shadow': '0px 8px 8px 8px',
-          '-webkit-transform': 'rotate(' + 30 + 'deg)',
-          '-moz-transform': 'rotate(' + 30 + 'deg)',
-          '-ms-transform': 'rotate(' + 30 + 'deg)',
-          'transform': 'rotate(' + 30 + 'deg)'
-        });
-        $("#box-sever").animate({
-          right: "16%",
-          top: "160px"
-        }, 1000);
+      if (windowWidth >= 1000) {
+        if (bxTwoTrigger <= window_top_position) {
+          $(".box").css({
+            "animation": "fadeIn ease 1s",
+            "-webkit-animation": "fadeIn ease 1s",
+            "-moz-animation": "fadeIn ease 1s",
+            "-o-animation": "fadeIn ease 1s",
+            "-ms-animation": "fadeIn ease 1s"
+          });
+          $("#box-one").animate({
+            left: "10%"
+          }, 1000);
+          $("#box-one").css({
+            '-webkit-transform': 'rotate(' + -30 + 'deg)',
+            '-moz-transform': 'rotate(' + -30 + 'deg)',
+            '-ms-transform': 'rotate(' + -30 + 'deg)',
+            'transform': 'rotate(' + -30 + 'deg)'
+          });
+          $("#box-two").animate({
+            left: "10%",
+            top: "40%"
+          }, 1000);
+          $("#box-three").animate({
+            right: "10%",
+            top: "15%"
+          }, 1000);
+          $("#box-four").animate({
+            right: "11%",
+            top: "35%"
+          }, 1000);
+          $("#box-five").animate({
+            right: "10%",
+            bottom: "25%"
+          }, 1000);
+          $("#box-six").animate({
+            left: "10%",
+            bottom: "25%"
+          }, 1000);
+          $("#box-six").css({
+            'box-shadow': '0px 8px 8px 8px',
+            '-webkit-transform': 'rotate(' + 30 + 'deg)',
+            '-moz-transform': 'rotate(' + 30 + 'deg)',
+            '-ms-transform': 'rotate(' + 30 + 'deg)',
+            'transform': 'rotate(' + 30 + 'deg)'
+          });
+          $("#box-sever").animate({
+            right: "16%",
+            top: "160px"
+          }, 1000);
+        }
       }
     };
 
